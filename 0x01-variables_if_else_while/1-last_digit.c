f0e3bf6597f97009c983fb6fecd2b531cfc248cb
Evedery20 <evedery20@gmail.com>
2022-10-28 10:29:17 -0700

    Positive

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- *
  * Return:always 0
- *
  */
 int main(void)
 { 
@@ -15,12 +13,12 @@ int main(void)
 	if (n < 0)
 	{
 		printf("%d is negative\n", n);
-	} else if (n == 0)
+	}  else if (n ==0)
 	{
 		printf("%d is zero\n", n);
-	} else 
+	}  else
 	{
 		printf("%d is positive\n", n);
 	}
 	return (0);
-}
+} 
