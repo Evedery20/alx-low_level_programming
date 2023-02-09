8aadf555fcd9c4fe3f525568af5a7e017e2b8953
Evedery20 <evedery20@gmail.com>
2023-02-09 05:25:35 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper  - checks for uppercase character
+ * @c: Variable text
+ * Return: Always 0
+ */
+
+in _isupper(int c)
+{
+	if(c >='A' && C <= 'z')
+
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+
+}
+		
