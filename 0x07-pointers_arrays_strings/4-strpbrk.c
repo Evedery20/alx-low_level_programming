e4c7b494b0aca6f723301a3f611cd663e5d85acb
Evedery20 <evedery20@gmail.com>
2023-03-05 21:15:01 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,21 +4,24 @@
  * _strpbrk - searches a string for any of a set of bytes
  * @s: string to check
  * @accept: string to check against
+ *
  * Return: pointer to byte in s that matches or NULL if no match
  */
+
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
-
-	for (i = 0; s[i]; i++)
+	int k;
+	while (*s)
 	{
-		for (j = 0; accept[j]; j++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (s[i] == accept[j])
-				break;
+			if (*s == accept[k])
+				return (s);
 		}
-		if (accept[j])
-			return (s + i);
+		s++;
 	}
-	return (0);
+	return ('\0');
 }
+
+
+
