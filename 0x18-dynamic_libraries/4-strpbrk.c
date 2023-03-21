88481900c7485c6c55bb0d73ea5a096427404a9c
Evedery20 <evedery20@gmail.com>
2023-03-20 18:31:08 -0700

    library luxury

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: pointer to byte in s that matches or NULL if no match
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
+
+
+
