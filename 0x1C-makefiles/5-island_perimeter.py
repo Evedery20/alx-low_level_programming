4c8551578a8f6f78ed2586dae36babcb7ec5b60b
Evedery20 <evedery20@gmail.com>
2023-05-24 16:12:04 -0700

    1c Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -24,6 +24,7 @@ def island_perimeter(grid):
                     edges += 1
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
-    return size 
+    return size * 4 - edges * 2
+
 
 
